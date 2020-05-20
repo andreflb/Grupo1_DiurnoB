751c7d2a429b631d0f25cfcaec17495f8b805208
BrunoPC <BrunoPC@Bruno.lan>
2020-05-20 17:53:22 +0100

    ola

--- ProjetoFinal/src/com/istec/main/OlaBruno.java
+++ ProjetoFinal/src/com/istec/main/OlaBruno.java
@@ -4,7 +4,9 @@ public class OlaBruno {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// OLA ANDRE
+    // OLA ANDRE
+		
+		System.out.println("ola andre");
 	}
 
 }
