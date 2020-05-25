17a2f881b19def4625c1b24e7e48863cede8506d
andreflb <andreflb@sapo.pt>
2020-05-25 17:08:23 +0100

    login

---
+++ ProjetoFinal/src/com/istec/swing/PlaceholderPasswordField.java
@@ -0,0 +1,39 @@
+package com.istec.swing;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JPasswordField;
+
+@SuppressWarnings("serial")
+public class PlaceholderPasswordField extends JPasswordField {
+	private String placeholder;
+	
+	public PlaceholderPasswordField() {
+		
+	}
+
+	public String getPlaceholder() {
+		return placeholder;
+	}
+
+	public void setPlaceholder(String placeholder) {
+		this.placeholder = placeholder;
+	}
+	
+	@Override
+	protected void paintComponent(final Graphics pG) {
+		super.paintComponent(pG);
+
+		if (placeholder == null || placeholder.length() == 0 || getText().length() > 0) {
+			return;
+		}
+
+		final Graphics2D g = (Graphics2D) pG;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setColor(getDisabledTextColor());
+		g.drawString(placeholder, getInsets().left, pG.getFontMetrics().getMaxAscent() + getInsets().top);
+	}
+
+}
